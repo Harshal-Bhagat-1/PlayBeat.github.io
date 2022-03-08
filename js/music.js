c6be830591e450275a58a3278f3e4fa34b0a3f96
Harshal-Bhagat-1 <bhagatharshal630@gmail.com>
2022-03-08 06:23:00 +0530

    Musica Website

---
+++ js/music.js
@@ -0,0 +1,169 @@
+    var sound=document.getElementById("audio");
+    var icon=document.getElementById("icon");
+    icon.onclick = function(){
+      if(sound.paused){
+        sound.play();
+        icon.src="images/pause.png";
+      }
+      else{
+        sound.pause();
+        icon.src="images/play2.png";
+    }
+}
+
+// 2nd song play
+    var sound1=document.getElementById("ik-kahani");
+    var icon1=document.getElementById("kahani");
+    icon1.onclick = function(){
+      if(sound1.paused){
+        sound1.play();
+        icon1.src="images/pause.png";
+      }
+      else{
+        sound1.pause();
+        icon1.src="images/play2.png";
+    }
+}
+
+
+// 3rd song play
+    var sound2=document.getElementById("sabki-barate-aayi");
+    var icon2=document.getElementById("sabki");
+    icon2.onclick = function(){
+      if(sound2.paused){
+        sound2.play();
+        icon2.src="images/pause.png";
+      }
+      else{
+        sound2.pause();
+        icon2.src="images/play2.png";
+    }
+}
+
+// 4th song play
+    var sound3=document.getElementById("srivalli");
+    var icon3=document.getElementById("srivali");
+    icon3.onclick = function(){
+      if(sound3.paused){
+        sound3.play();
+        icon3.src="images/pause.png";
+      }
+      else{
+        sound3.pause();
+        icon3.src="images/play2.png";
+    }
+}
+
+//---- English Songs ------------------------------------------------
+// 1st song
+    var sound4=document.getElementById("lily");
+    var icon4=document.getElementById("lily1");
+    icon4.onclick = function(){
+      if(sound4.paused){
+        sound4.play();
+        icon4.src="images/pause.png";
+      }
+      else{
+        sound4.pause();
+        icon4.src="images/play2.png";
+    }
+}
+
+// 2 song
+    var sound5=document.getElementById("cheap-thrills");
+    var icon5=document.getElementById("cheap");
+    icon5.onclick = function(){
+      if(sound5.paused){
+        sound5.play();
+        icon5.src="images/pause.png";
+      }
+      else{
+        sound5.pause();
+        icon5.src="images/play2.png";
+    }
+}
+
+// 3 song
+    var sound6=document.getElementById("dharia");
+    var icon6=document.getElementById("dharia1");
+    icon6.onclick = function(){
+      if(sound6.paused){
+        sound6.play();
+        icon6.src="images/pause.png";
+      }
+      else{
+        sound6.pause();
+        icon6.src="images/play2.png";
+    }
+}
+
+// 4 song
+    var sound7=document.getElementById("falling");
+    var icon7=document.getElementById("falling1");
+    icon7.onclick = function(){
+      if(sound7.paused){
+        sound7.play();
+        icon7.src="images/pause.png";
+      }
+      else{
+        sound7.pause();
+        icon7.src="images/play2.png";
+    }
+}
+
+// 5 song
+    var sound8=document.getElementById("havana");
+    var icon8=document.getElementById("havana1");
+    icon8.onclick = function(){
+      if(sound8.paused){
+        sound8.play();
+        icon8.src="images/pause.png";
+      }
+      else{
+        sound8.pause();
+        icon8.src="images/play2.png";
+    }
+}
+
+// 6 song
+    var sound9=document.getElementById("into-your-arms");
+    var icon9=document.getElementById("into");
+    icon9.onclick = function(){
+      if(sound9.paused){
+        sound9.play();
+        icon9.src="images/pause.png";
+      }
+      else{
+        sound9.pause();
+        icon9.src="images/play2.png";
+    }
+}
+
+// 7 song
+    var sound10=document.getElementById("let-me-down");
+    var icon10=document.getElementById("let");
+    icon10.onclick = function(){
+      if(sound10.paused){
+        sound10.play();
+        icon10.src="images/pause.png";
+      }
+      else{
+        sound10.pause();
+        icon10.src="images/play2.png";
+    }
+}
+
+// 8 song
+    var sound11=document.getElementById("levitating");
+    var icon11=document.getElementById("levitating1");
+    icon11.onclick = function(){
+      if(sound11.paused){
+        sound11.play();
+        icon11.src="images/pause.png";
+      }
+      else{
+        sound11.pause();
+        icon11.src="images/play2.png";
+    }
+}
+
